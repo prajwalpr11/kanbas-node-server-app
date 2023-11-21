74d93a21ca03a1b6e4d8fc252559dca6a7e0d12a
Prajwal Prasad <prajwalpr11@gmail.com>
2023-11-20 20:58:21 -0500

    backend

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js"
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors()
+);
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app)
+Hello(app)
+app.listen(process.env.PORT || 4000);
