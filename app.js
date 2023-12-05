59ea176d9e0facda3e93b64161aa775a3d6e35e2
Prajwal Prasad <prajwalpr11@gmail.com>
2023-12-04 19:16:25 -0500

    Node

--- app.js
+++ app.js
@@ -2,16 +2,41 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
-import CourseRoutes from "./courses/routes.js"
+import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
 import "dotenv/config";
 
 const app = express();
 app.use(express.json());
-app.use(cors()
+app.use(
+  cors({
+    credentials: true,
+    //origin: process.env.FRONTEND_URL,
+  })
 );
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
+
 CourseRoutes(app);
 ModuleRoutes(app);
-Lab5(app)
-Hello(app)
+Lab5(app);
+Hello(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
